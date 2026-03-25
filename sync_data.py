f46541cf5426d7f7f7cf08fe5d1967525ca99de4
butihu96 <butihu96@gmail.com>
2026-03-25 11:28:08 +0700

    Update sync_data.py

--- sync_data.py
+++ sync_data.py
@@ -177,7 +177,8 @@ def sync_data():
                 if config["type"] == "kho_4":
                     blocks = []
                     if i == 0:   blocks = [(1, 2, 3), (6, 7, 8), (11, 12, 13)]
-                    elif i == 1: blocks = [(0, 1, 2), (4, 5, 6), (9, 10, 11), (14, 15, 16)]
+                    # ĐÃ CẬP NHẬT TỌA ĐỘ CHUẨN CHO TAB 2 (Pickleball+DC) THEO ĐÚNG FORM SẾP GIAO
+                    elif i == 1: blocks = [(1, 2, 3), (6, 7, 8), (10, 11, 12), (14, 15, 16)]
                     elif i in [2, 3]: blocks = [(1, 2, 3), (6, 7, 8), (11, 12, 13)]
                     elif i in [4, 5]: blocks = [(1, 2, 3), (6, 7, 8)]
 
